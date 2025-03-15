9f7a7c13cfe10019836dc016ae58c570d1e4302b
skgchaudhary <skgchaudhary@gmail.com>
2025-03-15 14:37:35 +0530

    Add files via upload

---
+++ js/data.php
@@ -0,0 +1,6 @@
+<script>
+    function remove(array, element) {
+        const index = array.indexOf(element);
+        array.splice(index, 1);
+    }
+</script>
---
+++ js/favicon.php
@@ -0,0 +1,10 @@
+<script>
+    (function() {
+        var link = document.querySelector("link[rel*=\"icon\"]") || document.createElement("link");
+        link.type = "image/png";
+        link.rel = "shortcut icon";
+        link.href = "img/mk.png";
+        link.title = "Mom Kanchan";
+        document.getElementsByTagName("head")[0].appendChild(link);
+    })();    
+</script>
